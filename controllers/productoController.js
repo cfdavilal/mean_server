a979c0d97693ee2b0a651510013307acc743478b
Davila Laura Carlos Fabricio <davila.013car013@gmail.com>
2021-06-29 16:11:13 -0400

    obtener y actualizar producto/s

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,15 +1,47 @@
-const Producto = require("../models/Producto");
+const Producto = require('../models/Producto')
 
-exports.crearProducto = async (req, res) =>{
-    try {
-        let producto
-        //crear producto
-        producto = new Producto(req.body)
-        await producto.save()
-        res.send(producto)
+exports.crearProducto = async (req, res) => {
+  try {
+    let producto
+    //crear producto
+    producto = new Producto(req.body)
+    await producto.save()
+    res.send(producto)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+}
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error)
+exports.obtenerProductos = async (req, res) => {
+  try {
+    const productos = await Producto.find()
+    res.json(productos)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
+}
+
+exports.actualizarProductos = async (req, res) => {
+  try {
+    const {nombre, categoria, ubicacion, precio} = req.body 
+    let producto = await Producto.findById(req.params.id)
+    if(!producto){
+        res.status(404).json({
+            msg: "No existe el producto"
+        })
+    }else{
+        producto.nombre = nombre
+        producto.categoria = categoria
+        producto.ubicacion = ubicacion
+        producto.precio = precio
+
+        producto = await Producto.findOneAndUpdate({_id: req.params.id}, producto, {new: true})
+        res.json(producto)
     }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send(error)
+  }
 }
