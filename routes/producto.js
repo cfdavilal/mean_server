bcf0f01a3994b8698302091a0e1907b3eafbf4f6
Davila Laura Carlos Fabricio <davila.013car013@gmail.com>
2021-06-29 15:28:24 -0400

    Estructura del proyecto

---
+++ routes/producto.js
@@ -0,0 +1,9 @@
+//rutas para producto
+const express = require('express')
+const router = express.Router()
+const productoController = require('../controllers/productoController')
+
+//api/productos
+router.post('/', productoController.crearProducto)
+
+module.exports = router
